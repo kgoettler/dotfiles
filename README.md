2d59157630102a39b0530a2774b71cf53443e806
Kenneth Goettler <kgoettler@pop-os.localdomain>
2019-07-07 19:55:41 -0400

    Updated README and deploy script

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# dotfiles
-Dotfiles for macOS and Linux systems
+# Dotfiles
+
+These are my personal dotfiles and configurations for macOS and Linux systems,
+along with a bash script for deploying them on new builds.
+
+## macOS
+
+- bash
+- .inputrc
+- jupyter console & qtconsole
+- neofetch
+- neovim
+- tmux
+
+## Linux
+
+- bash
+- compton
+- conky
+- .inputrc
+- firefox
+- i3
+- jupyter console & qtconsole
+- neofetch
+- neovim
+- polybar
+- rofi
+- tmux
--- deploy.sh
+++ deploy.sh
@@ -22,23 +22,8 @@ for dotfile in "${dotfiles[@]}"; do
     outfile="${HOME}/${dotfile}"
     outdir=$(dirname ${outfile})
     if [ ! -d ${outdir} ]; then
-        echo "Directory does not exist"
-        #mkdir -p ${outdir}
+        mkdir -p ${outdir}
     fi
     ln -sf ${infile} ${outfile}
 done
 
-
-#ln ./.jupyter/jupyter_qtconsole_config.py ~/.jupyter/jupyter_qtconsole_config.py 
-#ln ./.jupyter/jupyter_console_config.py ~/.jupyter/jupyter_console_config.py
-#ln ./.bash_profile ~/.bash_profile
-#ln ./.vimrc ~/.vimrc
-#ln ./.inputrc ~/.inputrc
-#ln ./.tmux.conf ~/.tmux.conf
-#ln ./.config/nvim/init.vim ~/.config/nvim/init.vim
-#ln ./.config/rofi/config ~/.config/rofi/config
-#ln ./.config/neofetch/config.conf ~/.config/neofetch/config.conf
-#ln ./.config/i3/config ~/.config/i3/config
-#ln ./.config/polybar/config ~/.config/polybar/config
-#ln ./.config/compton.conf ~/.config/compton.conf
-#ln ./.conky/.conkyrc ~/.config/.conky/.conkyrc
